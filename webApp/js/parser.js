8ae064b4d73af3a11b98c5725ddf77f37ea1b6a5
melinaz@live.com <melinaz@live.com>
2021-08-03 19:56:42 +0300

    condition parser - initial commit

---
+++ webApp/js/parser.js
@@ -0,0 +1,212 @@
+// ex = "age (years) <= 64 AND age (years) >= 60 AND sex = 'male'";
+// ex = ( body mass index < 40 AND body mass index >= 35 ) AND sex = 'female'
+
+var RiskEvidenceConditionParser = {
+	
+    operators: ["!=", "=", ">=", "<=", ">", "<", "OR", "AND"],
+
+    // parse and evaluate an expression 
+    // (ex. sex = 'female' AND age > 30)
+    parseAndEvaluate: function(ex) {
+        var array = ex.split("");
+        for (var i = 0; i < array.length; i++) {
+            
+            // if the parsed array is not empty
+            // continue with the evaluation of the strings
+            if (array[i] !== " "){
+                return this.parse(ex);
+            } 
+
+            console.error("ERROR: Expression cannot be empty!");
+            return false;
+        }
+    },
+
+    evaluate: function(condition, vars) {
+        // sort by key length
+        var sortedVals = Object.keys(vars).sort(function(a,b){return b.length>a.length;});
+        sortedVals.forEach(function(val){
+            condition=RiskEvidenceConditionParser.replaceAll(condition,val+" ",vars[val]);
+        });
+        
+		//error control
+		if(condition.indexOf("OB_")>=0) {
+// 			console.error("===ERROR in OB replacement===");
+// 			console.error("--Condition:",condition);
+// 			console.error("--Observable:",
+// 			condition.substr(condition.indexOf("OB_"),5));
+			return false;
+		}
+        return this.parseAndEvaluate(condition);
+    },
+
+    parse: function(ex) {
+        var op = this.operatorDetails(ex);
+        var start = op[0];
+        var left = s.substring(0, start).trim();
+        var right = s.substring(op[1]).trim();
+        var oper = s.substring(start, op[1]).trim();
+        var opType = this.logicalOperatorType(oper);
+
+        // console.log("PARSE: Left: \"" + left + "\" Right: \"" + right + "\" Operator: \"" + oper + "\"");
+
+        if (opType === 0){
+            return this.parse(left) || this.parse(right);
+        }
+
+        else if (opType === 1){
+            return this.parse(left) && this.parse(right);
+        } 
+
+        var left = this.removeParens(left);
+        var right = this.removeParens(right);
+
+        if (this.isNumeric(left) && this.isNumeric(right)){
+            return this.evaluateFloat(parseFloat(left), oper, parseFloat(right));
+        }
+        else {
+            return this.evaluateStr(left, oper, right); // assume they are strings
+        }
+    },
+
+    // determine operator precedence and location
+    operatorDetails: function(ex) {
+        ex = ex.trim();
+
+        var minParens = 999999999999;
+        var currentMin = null;
+
+        for (var size = 1; size <= 3; size++) {
+            for (var location = 0; location < (ex.length + 1) - size; location++) {
+
+                var endIndex = location + size;
+                var sub;
+
+                if ((endIndex < ex.length && ex[endIndex] === '=')){
+                    sub = ex.substring(location, 2 + endIndex).trim();
+                }
+
+                else {
+                    sub = ex.substring(location, endIndex).trim();
+                }
+
+                if (this.isOperator(sub)) {
+                    
+                    // Idea here is to weight logical operators so that they will still be selected over other operators
+                    // when no parens are present
+                    var parens = (this.logicalOperatorType(sub) > -1) ? this.parens(ex, location) - 1 : this.parens(s, locInStr);
+
+                    if (parens <= minParens) {
+
+                        minParens = parens;
+                        currentMin = [location, endIndex, parens];
+                    }
+                }
+            }
+        }
+
+        return currentMin;
+    },
+
+    // return 0  : if operator (op) == OR
+    // return 1  : if operator (op) == AND
+    // return -1 : otherwise
+    logicalOperatorType: function(op) {
+        if (op.trim() === "OR") {
+            return 0;
+        }
+        else if (op.trim() === "AND") {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    },
+
+    parens: function(ex, location) {
+
+        var parens = 0;
+
+        for (var i = 0; i < ex.length; i++){
+
+            if (ex[i] === '(' && i < location){
+                parens++;
+            }
+
+            if (ex[i] === ')' && i >= location){
+                parens++;
+            }
+        }
+
+        return parens;
+    },
+
+    // remove parentheses from expression
+    removeParens: function(s) {
+
+        s = s.trim();
+        var keep="";
+
+        s.split("").forEach(function(c){
+
+            if (!(c === '(') && !(c === ')'))
+                keep += c;
+        });
+
+        return keep.toString().trim();
+    },
+    
+    replaceAll: function(target, search, replacement) {
+    	return target.split(search).join(replacement);
+	},
+
+    isOperator: function(op) {
+        return this.operators.indexOf(op.trim())>=0;
+    },
+
+    isNumeric: function(s) {
+  		return !isNaN(parseFloat(s)) && isFinite(s);
+    },
+
+    evaluateFloat: function(left, op, right) {
+
+        if (op === "=") {
+            return left === right;
+        }
+        else if (op === ">") {
+            return left > right;
+        }
+        else if (op === "<") {
+            return left < right;
+        }
+        else if (op === "<=") {
+            return left <= right;
+        }
+        else if (op === ">=") {
+            return left >= right;
+        }
+        else if (op === "!=") {
+            return left != right;
+        }
+        else {
+            console.error("ERROR: Operator type not recognized.");
+            return false;
+        }
+    },
+    
+    fixQuotes: function(str){
+        return str.replace(/['"]+/g, '');   
+    },
+
+    evaluateStr: function(left, op, right) {
+
+        if (op === "=" || op=== "!=" ) {
+            return this.fixQuotes(left) === this.fixQuotes(right);
+        } 
+        
+        else {
+            console.error("ERROR: Operator type not recognized." + left + " " +  op +  " " + right);
+            return false;
+        }
+    }
+};
