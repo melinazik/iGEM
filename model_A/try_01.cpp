34ea3ef1dfdd137b7161343bf1cab50615537078
NefeliZ <nefeliz@live.com>
2021-09-03 00:37:29 +0300

    model A equations - constants still wrong(closer to reality tho)- problem with old&new values fixed

--- model_A/try_01.cpp
+++ model_A/try_01.cpp
@@ -1,69 +1,80 @@
 #include <iostream> //sta default folders gia libraries
-#include <chrono>
-#include <thread>
 #include <math.h>
 #include <string>
 #include <fstream>
 
-#include <list>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
-double k_syn = 666.0;
-double c1 = 666.0;
+//assumption: [miRNA]/t equally distributed to num of exosomes/t
+
+double k_syn = 0.7;
+double c1 = 0.6;
 double c2 = 20.6;
-double mRNA = 0;
+
 double DmRNA = 0.41; // m mol / min 
 double a_prot = 720.0; // aminoacids / min
-double L = 666.0; // mikos
+double L = 680.0; // mikos
 double D_prot = 0.0000139; // mol / min
 double k0 = 79000; // min ^ -1
-double n0 = 666.0; // starting quantity of miRNA
-double n1 = 666.0; // exosome number
-double k1 = 666.0; // degradation rate of complex / target
-double kts = 666.0; //idk
-// double kts = 666.0; //idk
+double n0 = 0; // starting quantity of miRNA
+double n1 = 1.0; // exosome number
+double k1 = 0.5; // degradation rate of complex / target
+double kts = 7.0; //idk
 
-double eq_mRNA1 (double n, double kts, double DmRNA, double k_syn, double t)
+//initialize
+double mRNA1 = 0;
+double mRNA2 = 0;
+double P = 0;
+double miRNA = 0;
+double target = 0;
+
+
+
+double eq_mRNA1 (double n, double kts, double DmRNA, double k_syn, double t, double mRNA1_prev)
 {
-    double mRNA1 = n* kts * t - DmRNA * t - k_syn * mRNA * t;
+    double mRNA1 = n* kts * t - DmRNA * t - k_syn * mRNA1_prev * t;
+
+    cout << "mRNA1 value after calc at t=" << t <<" is " << mRNA1 <<endl;
 
     return mRNA1;
 }
 
-double eq_miRNA (double c1, double tx0,  double mRNA1, double k_syn, double t)
+double eq_miRNA (double c1, double mRNA1, double k_syn, double Exo, double t, double miRNA_prev) //sugkentrosi miRNA per t 
 {
-    double miRNA = k_syn * mRNA1 * t - c1 * tx0 * miRNA * t;
+    double miRNA = k_syn * mRNA1 * t - c1 *  miRNA_prev * t; //*Exo
 
     return miRNA;
 }
 
-double eq_P (double a_prot, double L, double mRNA1, double mRNA2, double D_prot, double c2, double tx0, double t)
+double eq_P (double a_prot, double L, double mRNA1, double mRNA2, double D_prot, double c2, double Exo,  double t, double P_prev) 
 {
-    double P = (a_prot/L) * mRNA1 * mRNA2 * t - D_prot * P * t - c2 * tx0 * P * t;
+
+    double P = (a_prot/L) * (mRNA1 + mRNA2) * t - D_prot * P_prev * t - c2 * P_prev * t; //*Exo
+
+    cout << "P value after calc at t=" << t <<" is " << P << endl;
 
     return P;
 }
 
-double eq_tx0 (double k0, double t)
+double eq_Exo (double k0, double t) //num of exosomes at t - k0 exosomes per t
 {
-    double tx0 = k0 * t;
+    double Exo = k0 * t;
 
-    return tx0;
+    return Exo;
 }
 
-double eq_target (double n0, double n1, double c1, double miRNA, double k1, double t)
+double eq_target (double n0, double n1, double c1, double miRNA, double k1, double t, double target_prev)
 {
-    double target = n0 * t + c1 * miRNA * n1 * t - k1 * target * t;
+    double target = n0 * t + c1 * miRNA * n1 * t - k1 * target_prev * t;
 
     return target;
 }
 
-double eq_mRNA2 (double k_syn, double mRNA1, double DmRNA, double t)
+double eq_mRNA2 (double k_syn, double mRNA1, double DmRNA, double t, double mRNA2_prev)
 {
-    double mRNA2 = k_syn * mRNA1 * t - DmRNA * mRNA2 * t;
+    double mRNA2 = k_syn * mRNA1 * t - DmRNA * mRNA2_prev * t;
 
     return mRNA2;
 }
@@ -73,41 +84,50 @@ int main()
     // initialize csv file
     std::ofstream file;
     file.open ("C:\\workspace\\iGEM\\----PROJECT-----\\DL\\model\\A\\code\\model_A_results_01.csv");
-    file << "time, mRNA1, miRNA, P, tx0, target, mRNA2\n";
+    file << "time, mRNA1, miRNA, P, Exo, target, mRNA2\n";
 
-    double mRNA1;
-    double miRNA;
-    double P;
-    double tx0;
-    double target;
-    double mRNA2;
+    double mRNA1 = 0;
+    double miRNA = 0;
+    double P = 0;
+    double Exo = 0;
+    double target = 0;
+    double mRNA2 = 0;
 
 
-    for (int t=0; t <10; t++)
+    for (int t=0; t <5; t=t+1)
     {
-        mRNA1 = eq_mRNA1 (1.0, kts, DmRNA, k_syn, t);
-        miRNA = eq_miRNA (c1, tx0, mRNA1, k_syn, t);
-        P = eq_P (a_prot, L, mRNA1, mRNA2, D_prot, c2, tx0, t);
-        tx0 = eq_tx0 (k0, t);
-        target = eq_target (n0, n1, c1, miRNA, k1, t);
-        mRNA2 = eq_mRNA2 (k_syn, mRNA1, DmRNA, t);
+        double mRNA1_prev = mRNA1;
+        double mRNA2_prev = mRNA2;
+        double miRNA_prev = miRNA;
+        double target_prev = target;
+        double P_prev = P;
+
+        cout << "for:mRNA1 pre calc " << mRNA1 << endl;
+        mRNA1 = eq_mRNA1 (1.0, kts, DmRNA, k_syn, t, mRNA1_prev);
+        cout << "for:mRNA1 post calc " << mRNA1 << endl;
+
+        cout << "for:mRNA1 pre calc for miRNA " << mRNA1 << endl;
+        miRNA = eq_miRNA (c1, Exo, mRNA1, k_syn, t, miRNA_prev);
+        cout << "for:mRNA1 post calc for miRNA " << mRNA1 << endl;
+
+        P = eq_P (a_prot, L, mRNA1, mRNA2, D_prot, c2, Exo, t,P_prev);
+        Exo = eq_Exo (k0, t);
+        target = eq_target (n0, n1, c1, miRNA, k1, t, target_prev);
+        mRNA2 = eq_mRNA2 (k_syn, mRNA1, DmRNA, t, mRNA2_prev);
 
         cout << "mRNA1 " << mRNA1 << endl;
         cout << "miRNA " << miRNA << endl;
         cout << "P " << P << endl;
-        cout << "tx0 " << tx0 << endl;
+        cout << "Exo " << Exo << endl;
         cout << "target " << target << endl;
         cout << "mRNA2 " << mRNA2 << endl;
 
-        //file << t; <<"\n"; // ","<< mRNA1<< "," << miRNA << ","<< P << ','<< tx0 << target << mRNA2 << "\n";
-
-        //file << t, "," mRNA1,",", miRNA,",", P,",", tx0, ",",target,",", mRNA2, "\n" ;
-
+        
         file << t << " ,";
         file << mRNA1 << " ,";
         file << miRNA << " ,";
         file << P << " ,";
-        file << tx0 << " ,";
+        file << Exo << " ,";
         file << target << " ,";
         file << mRNA2 << " , \n" ;
         
