a64ceb33722b7318acd96ee509923e7ad790c75d
melinaz@live.com <melinaz@live.com>
2021-09-05 22:47:03 +0300

    add questions to quiz

--- educationalTool/js/quiz.js
+++ educationalTool/js/quiz.js
@@ -1,5 +1,248 @@
 const questions = [
-    
+    {
+        question: "Which organel is the powerhouse of the cell?",
+        optionA: "Golgi",
+        optionB: "Endoplasmatic reticulum",
+        optionC: "Mitochondria",
+        optionD: "Nucleus",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which is the most common type of Arthritis",
+        optionA: "Gout",
+        optionB: "Osteoarthritis",
+        optionC: "Rheumatoid Arthritis",
+        optionD: "Psoriatic Arthritis",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "How much time does it take for an E.coli cell to replicate?",
+        optionA: "30 minutes",
+        optionB: "20 minutes",
+        optionC: "1 hour",
+        optionD: "12 hours",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "__ has the biggest number of chromosomes.",
+        optionA: "Corn",
+        optionB: "Humans",
+        optionC: "African Hedgehog",
+        optionD: "Fruit Fly",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "The word NOUS in Greek means...",
+        optionA: "Mind",
+        optionB: "Thought",
+        optionC: "Unity",
+        optionD: "Alive",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Where is the smallest bone in human body located?",
+        optionA: "Toes",
+        optionB: "Ears",
+        optionC: "Fingers",
+        optionD: "Nose",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Rhizobia are bacteria known for fixing what type of gas?",
+        optionA: "Nitrogen",
+        optionB: "Carbon Dioxide",
+        optionC: "Oxygen",
+        optionD: "Hydrogen",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Who discovered bacteria in 1674?",
+        optionA: "Francesco Redi",
+        optionB: "Robert Koch",
+        optionC: "Antoni van Leeuwenhoek",
+        optionD: "Louis Pasteur",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which chemical inside the nucleus of a cell carries the genetic instructions for making living organisms?",
+        optionA: "DNA",
+        optionB: "Nucleolus",
+        optionC: "Cytoplasm",
+        optionD: "Nucleoplasm",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "How many blood cells are destroyed every second in the human body?",
+        optionA: "5 million",
+        optionB: "20 billion",
+        optionC: "30 million",
+        optionD: "15 million",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What allows plants to absorb energy from light?",
+        optionA: "Chroloplast",
+        optionB: "Chlorophyl",
+        optionC: "Stem",
+        optionD: "Leaves",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "The first biotechnology patent was for a microorganism used to clean up what?",
+        optionA: "Oil spills",
+        optionB: "Vacuum cleaners",
+        optionC: "Bedrooms",
+        optionD: "Cafeterias",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "One of the most famous examples of the practical use of synthetic biology was the modification of E. coli and yeast to produce a drug to fight what disease?",
+        optionA: "Scurvy",
+        optionB: "Rickets",
+        optionC: "Cancer",
+        optionD: "Malaria",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Who received the Nobel Prize in Chemistry in 1993 for inventing PCR?",
+        optionA: "David Baltimore",
+        optionB: "Kary Mullis",
+        optionC: "Robert Weinberg",
+        optionD: "John Nash",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "What does the acronym PCR stand for?",
+        optionA: "Polymerase Chain Reaction",
+        optionB: "Polymerase Copying Reaction",
+        optionC: "Polymerase Copied Repeats",
+        optionD: "Polymerase Chain Restoration",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Nucleotide triphosphates (dNTPs) are added to the growing DNA strand during the _______________ phase. ",
+        optionA: "Extension/elongation",
+        optionB: "Annealing",
+        optionC: "Denaturation/separation",
+        optionD: "Preparation",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Where does DNA replication take place?",
+        optionA: "Cytoplasm",
+        optionB: "Ribosome",
+        optionC: "Nucleus",
+        optionD: "Golgi Body",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "The process of making mRNA from DNA is called:",
+        optionA: "Replication",
+        optionB: "Protein synthesis",
+        optionC: "Translation",
+        optionD: "Transcription",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "A triplet of mRNA is called:",
+        optionA: "codon",
+        optionB: "amino acid",
+        optionC: "anticodon",
+        optionD: "peptide bond",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "DNA consists of bases. What holds the base pairs together?",
+        optionA: "peptide bonds",
+        optionB: "hydrogen bonds",
+        optionC: "ionic bonds",
+        optionD: "covalent bonds",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "The enzyme that catalyzes the synthesis of new DNA molecules is called:",
+        optionA: "Scurvy",
+        optionB: "Rickets",
+        optionC: "Cancer",
+        optionD: "Malaria",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "One of the most famous examples of the practical use of synthetic biology was the modification of E. coli and yeast to produce a drug to fight what disease?",
+        optionA: "DNA ligase",
+        optionB: "DNA gyrase",
+        optionC: "DNA polymerase",
+        optionD: "DNA helicase",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following is NOT a component of nucleic acids?",
+        optionA: "nitrogen bases",
+        optionB: "protein",
+        optionC: "phosphate",
+        optionD: "sugar",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "The double helix DNA model was proposed by:",
+        optionA: "Watson and Crick",
+        optionB: "Griffith",
+        optionC: "Avery",
+        optionD: "Beadle and Tatum",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "If a short sequence of DNA is AATTGCCGT, its complement is:",
+        optionA: "AAAACDCCA",
+        optionB: "TTAACGGCT",
+        optionC: "TTAACGGCA",
+        optionD: "TTAAGCCGA",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "The function of tRNA is to:",
+        optionA: "provide a place for protein synthesis",
+        optionB: "transport amino acids to the ribosome",
+        optionC: "travel to the ribosome to direct protein synthesis",
+        optionD: "none of these is correct",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Amino acids are the building blocks of:",
+        optionA: "DNA and RNA",
+        optionB: "lipids",
+        optionC: "proteins",
+        optionD: "carbohydrates",
+        correctOption: "optionC"
+    },
+
+
 
 ]
 
