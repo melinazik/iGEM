5b7805a503c0575d8887debfe2f517e5bc22c387
melinaz@live.com <melinaz@live.com>
2021-09-28 23:44:58 +0300

    fixed include from js file

--- educationalTool/js/ppt.js
+++ educationalTool/js/ppt.js
@@ -1,25 +1,30 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-            
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 function openPPT() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+                
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
  function closePPT(){
-  modal.style.display = "none";
+     // Get the modal
+    var modal = document.getElementById("myModal");
+                
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
